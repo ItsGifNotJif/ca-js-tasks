978026b2d6ca5d6794bd6e14b67447e1d20eddd7
ItsGifNotJif <115978042+ItsGifNotJif@users.noreply.github.com>
2022-11-25 12:46:56 +0200

    Add files via upload

---
+++ week 1/pamoka 27/main.js
@@ -0,0 +1,10 @@
+
+
+const numbers = [4, 6, 8, 2, -244, 0, 420, -690, 4, 1, 4, 20, 11, 42, 2, -5, -20, -199 ]
+    
+console.log('-----------')
+
+for (let i = numbers.length; i--;){
+    console.log(numbers[i]);
+}
+
